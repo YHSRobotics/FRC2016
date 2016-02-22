2f275381af0b02ea76c8988b6ce0a0bfea8ab351
MicahRaney <micah.raney@gmail.com>
2016-02-21 20:14:57 -0800

    Added support for partial power
    Both the AngleTools library and the DriveStraight command now have the
    capability to act at partial power. DriveStraight also can now drive
    backwards.

--- src/org/usfirst/frc/team4161/robot/commands/AngleTools.java
+++ src/org/usfirst/frc/team4161/robot/commands/AngleTools.java
@@ -3,40 +3,74 @@ package org.usfirst.frc.team4161.robot.commands;
 public class AngleTools {
 	/**
 	 * compute the power setting if the robot is angleDifference degrees away
-	 * from target.
+	 * from target. Essentially calls computePower(angleDifference, 0.1, 1)
 	 * 
-	 * @param angleDifference The difference in angle
+	 * @param angleDifference
+	 *            The difference in angle
 	 * @return The power in the range of [.1, 1]
 	 */
 	public static double computePower(double angleDifference) {
-		
-		if(angleDifference < 0)
-			angleDifference *= -1;//make sure it is positive.
-		double power = Math.pow(2, angleDifference/15) - 1;//use the power equation
-		if (power > 1)
-			return 1;//power is over 1
-		else if (power > .1)//power is between .1 and 1
+
+		return computePower(angleDifference, 0.1, 1);
+
+	}
+	/**
+	 * compute the power setting if the robot is angleDifference degrees away
+	 * from target. Essentially calls computePower(angleDifference, 0.1, maxPower)
+	 * 
+	 * @param angleDifference
+	 *            The difference in angle
+	 * @param maxPower The maximum power value to return.
+	 * @return The power in the range of [.1, maxPower]
+	 */
+	public static double computePower(double angleDifference, double maxPower){
+		return computePower(angleDifference, 0.1, 1);
+	}
+	
+	/**
+	 * Compute the power setting of the robot when approaching a desired
+	 * heading/distance. The lower the angleDifference, the lower the computed
+	 * power. The returned power is in the range [minPower, maxPower].
+	 * 
+	 * @param angleDifference The difference between the heading/bearing of the robot.
+	 * @param minPower Minimum power to return.
+	 * @param maxPower Maximum power to return.
+	 * @return
+	 */
+	public static double computePower(double angleDifference, double minPower, double maxPower) {
+
+		if (angleDifference < 0)
+			angleDifference *= -1;// make sure it is positive.
+		double power = Math.pow(2, angleDifference / 15) - 1;// use the power
+																// equation
+		if (power > maxPower)
+			return maxPower;// power is over 1
+		else if (power > minPower)// power is between .1 and 1
 			return power;
-		else//power is below 1
-			return .1;
+		else// power is below 1
+			return minPower;
 
 	}
 
 	/**
-	 * Determine the angle difference between the heading and the bearing. (in degrees)
-	 * @param heading Heading of the robot
-	 * @param bearing Desired bearing.
+	 * Determine the angle difference between the heading and the bearing. (in
+	 * degrees)
+	 * 
+	 * @param heading
+	 *            Heading of the robot
+	 * @param bearing
+	 *            Desired bearing.
 	 * @return Difference in heading in range (-180, 180]
 	 */
-	public static double getAngleDifference(double heading, double bearing){
+	public static double getAngleDifference(double heading, double bearing) {
 		double ret = bearing - heading;
-		while(ret <= -180)//get it above -180
+		while (ret <= -180)// get it above -180
 			ret += 360;
-		while (ret > 180)//get it below 180
+		while (ret > 180)// get it below 180
 			ret -= 360;
 		return ret;
 	}
-	
+
 	/**
 	 * get the angle within the range of [0, 360)
 	 * 
@@ -51,5 +85,5 @@ public class AngleTools {
 			angle += 360;
 		return angle;
 	}
-	
+
 }
--- src/org/usfirst/frc/team4161/robot/commands/DriveStraight.java
+++ src/org/usfirst/frc/team4161/robot/commands/DriveStraight.java
@@ -11,52 +11,77 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class DriveStraight extends Command {
-	
+
 	private DriveTrain driveTrain = Robot.driveTrain;
 	private AnalogGyro gyro = RobotMap.gyro;
 
 	private int ticks;
-	private double heading;
+	private boolean backwards;
+	private double heading, maxPower;
+
 	/**
 	 * Drive straight forward for x number of ticks.
-	 * @param ticks Number of ticks to drive forward for.
+	 * 
+	 * @param ticks
+	 *            Number of ticks to drive forward for.
+	 * @param forward If true, drive forward. Else drive backwards.
 	 */
-    public DriveStraight(int ticks) {
-    	this.ticks = ticks;
-    	requires(driveTrain);//uses the drive train.
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public DriveStraight(int ticks, boolean forward) {
+		this(ticks, forward ? 1 : -1);//call the other constructor.
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	driveTrain.setDrive(1, 1);
-    	heading = RobotMap.gyro.getAngle();
-    }
+	/**
+	 * Makes the robot drive straight, but does not exceed maxPower. If maxPower
+	 * is negative, it will drive backwards.
+	 * 
+	 * @param ticks
+	 *            Ticks to drive for.
+	 * @param maxPower Maximum power to use. Must be in range [0, 1] (NOTE: 0 will not go anywhere).
+	 */
+	public DriveStraight(int ticks, double maxPower) {
+		requires(driveTrain);
+		this.ticks = ticks;
+		if(maxPower < 0){
+			maxPower = -1 * maxPower;//make the power positive.
+			backwards = true;//go backwards
+		}
+		else
+			backwards = false;//go forwards.
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	ticks--;
-    	double diff = AngleTools.getAngleDifference(gyro.getAngle(), heading);
-    	if(diff < -1)//correct left
-    		driveTrain.setDrive(1 - AngleTools.computePower(diff), 1);
-    	else if(diff > 1)//correct right.
-    		driveTrain.setDrive(1, 1 - AngleTools.computePower(diff));
-    	else
-    		driveTrain.setDrive(1, 1);//go straight.
-    }
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return ticks <= 0;
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		heading = RobotMap.gyro.getAngle();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		ticks--;
+		double diff = AngleTools.getAngleDifference(gyro.getAngle(), heading);
+		double compPower = 1 - AngleTools.computePower(diff, maxPower),//get the computed power
+				regPower = backwards ? -1*maxPower:maxPower;//get 'regular' power.
+		compPower = backwards ? -1*compPower : compPower;//check for backwards.
+		
+		if (diff < -1)// correct left
+			driveTrain.setDrive(compPower, regPower);
+		else if (diff > 1)// correct right.
+			driveTrain.setDrive(regPower, compPower);
+		else
+			driveTrain.setDrive(regPower, regPower);// go straight.
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return ticks <= 0;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
