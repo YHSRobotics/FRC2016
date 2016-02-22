035ddd65aa52891bc547c1cc1531ff46682350ca
MicahRaney <micah.raney@gmail.com>
2016-02-21 21:01:34 -0800

    Added ConversionFactor for helpful conversions.
    Use the ConversionFactor methods to convert from real world measurements
    (ie feet) into a number of ticks for a command such as DriveStraight.
    For example, if you want to drive straight for 5 feet, you can use the
    command DriveStraight(ConversionFactor.feetToTick(5), true)

---
+++ src/org/usfirst/frc/team4161/robot/ConversionFactor.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team4161.robot;
+
+public class ConversionFactor {
+
+	public static double ticksToFeet = 1, ticksToDegree = 1;
+
+	/**
+	 * Given the desired degree, returns the number of (estimated) ticks to turn
+	 * the arm for at full power.
+	 * 
+	 * @param degrees
+	 *            Desired degree of arm turn. Negatives are ignored
+	 * @return The number of (estimated) ticks to turn arm for.
+	 */
+	public static int degreeToTick(double degrees) {
+		return (int) Math.round(Math.abs(degrees) * ticksToDegree);
+	}
+
+	/**
+	 * Given the desired distance, returns the number of (estimated) ticks to
+	 * drive robot at full power.
+	 * 
+	 * @param feet
+	 *            Desired distance to drive robot. Negatives are ignored
+	 * @return The number of (estimated) ticks to drive for.
+	 */
+	public static int feetToTick(double feet) {
+		return (int) Math.round(Math.abs(feet) * ticksToFeet);
+	}
+
+}
