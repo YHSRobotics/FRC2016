124a7479ecdec0aa3500c2411d7cf9924cb960df
MicahRaney <MicahRaney@users.noreply.github.com>
2016-03-30 17:11:26 -0700

    Clarified ConversionFactor Methods

--- src/org/usfirst/frc/team4161/robot/ConversionFactor.java
+++ src/org/usfirst/frc/team4161/robot/ConversionFactor.java
@@ -2,7 +2,19 @@ package org.usfirst.frc.team4161.robot;
 
 public class ConversionFactor {
 
-	public static double ticksToFeet = 1, ticksToDegree = 10;
+	public static double ticksToFeet = 1, driveTicksToDegree = 10, armTicksToDegree = 10;
+
+	/**
+	 * Given the desired degree, returns the number of (estimated) ticks to
+	 * drive the robot for (at full power)
+	 * 
+	 * @param degrees
+	 *            Desired degree of arm turn. Negatives are ignored
+	 * @return The number of (estimated) ticks to turn arm for.
+	 */
+	public static int driveDegreesToTick(double degrees) {
+		return (int) Math.abs(Math.round(Math.abs(degrees) * driveTicksToDegree));
+	}
 
 	/**
 	 * Given the desired degree, returns the number of (estimated) ticks to turn
@@ -12,8 +24,8 @@ public class ConversionFactor {
 	 *            Desired degree of arm turn. Negatives are ignored
 	 * @return The number of (estimated) ticks to turn arm for.
 	 */
-	public static int degreeToTick(double degrees) {
-		return (int) Math.abs(Math.round(Math.abs(degrees) * ticksToDegree));
+	public static int armDegreesToTick(double degrees) {
+		return (int) Math.abs(Math.round(Math.abs(degrees) * armTicksToDegree));
 	}
 
 	/**
